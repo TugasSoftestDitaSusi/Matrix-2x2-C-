53278221131f15dc01a9a1b1417dfcf8034e4664
ditapaski <ditta270795@gmail.com>
2016-12-09 20:18:05 +0700

    Add files via upload

---
+++ matrix2.h
@@ -0,0 +1,30 @@
+Senin	08:00 - 09:40	Teknik Simulasi & Pemodelan
+		10:00 - 11:40	Analisis Perancangan Sistem
+		
+Selasa	09:00 - 10:30	Sistem Mikroprosessor 		[PRATIKUM]
+		13:00 - 14:40	Desain Grafis & Multimedia
+		15:00 - 16:40	Sistem Mikroprosessor 
+	
+Rabu	10:00 - 12:00	Pengolah Citra
+		15:00 - 16:40	KWH
+	
+Kamis	07:30 - 09:00	Desain Grafis & Multimedia	[PRATIKUM]
+
+Jumat	09:00 - 10:30	Pengolah Citra 				[PRATIKUM]
+
+Sabtu	08:00 - 09:40	Software Testing
+		10:00 - 11:40 	Manajemen Proyek
+		14:30 - 16:00	Teknik Simulasi & Pemodelan	[PRATIKUM]
+		
+Jadwal UTS
+(07-11-16)
+Senin	15:00 - 16:30	Analisis Perancangan Sistem		RK. 1.3.3
+Selasa	13:20 - 14:50	KWH								RK. 1.3.3 
+Rabu	13:20 - 14:50	Teknik Simulasi & Pemodelan		RK. 1.3.3 
+Sabtu	09:40 - 11:10	Sistem Mikroprosessor			RK. 1.3.3 
+
+(14-11-16)
+Senin 	11:30 - 12:50	Manajemen Proyek				RK. 1.3.3
+Rabu	13:20 - 14:40	Software Testing				RK. 1.3.3 
+Kamis	15:00 - 16:30	Pengolah Citra					GPS 3A	RK.4-7
+Jumat	08:00 - 09:30	Desain Grafis & Multimedia		RK. 1.3.3
